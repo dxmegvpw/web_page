55f711d2c8b8f950eb732edfc92fccf10d955d20
imiya <itsuki.miyane@gmail.com>
2024-06-25 22:48:12 +0900

    update files

---
+++ ind/head.php
@@ -0,0 +1,6 @@
+<title>物置</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/web_page/favicon.ico">
+    <link rel="stylesheet" href="/web_page/style.css">
+
+    <script type="text/javascript" id="MathJax-script" async
+        src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
